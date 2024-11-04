2a775a4787ad8dc35fac76a737c98e617f7ffc53
ttmcoder <trumanjmeyer@gmail.com>
2024-11-04 14:55:59 -0500

    Create style.css

---
+++ style.css
@@ -0,0 +1,30 @@
+html {
+  height: 100%;
+  width: 100%;
+}
+
+.box {
+  height: 450px;
+  width: 750px;
+  border: solid 3px black;
+}
+
+.mountain {
+  background-image: url("https://images.unsplash.com/photo-1632714394543-f7f797db5ee5?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bW91bnRhaW4lMjBzY2VuZXJ5fGVufDB8fDB8fHww");
+  background-size: cover;
+}
+
+.ocean {
+  background-image: url("https://images.pexels.com/photos/457881/pexels-photo-457881.jpeg?cs=srgb&dl=pexels-asadphoto-457881.jpg&fm=jpg");
+  background-size: cover;
+}
+
+.forest {
+  background-image: url("https://images.unsplash.com/photo-1542273917363-3b1817f69a2d?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGRhcmslMjBncmVlbiUyMGZvcmVzdHxlbnwwfHwwfHx8MA%3D%3D");
+  background-size: cover;
+}
+
+.desert {
+  background-image: url("https://images3.alphacoders.com/686/68605.jpg");
+  background-size: cover;
+}
