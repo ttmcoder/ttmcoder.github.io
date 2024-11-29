e77e0c03306dc7cdf79f26e539e9e6ab281cd5be
ttmcoder <trumanjmeyer@gmail.com>
2024-11-29 14:24:13 -0500

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function showit(var1) {
-  // Hide the one div that is shown
-  document.getElementById('page1').style.display = 'none';
-  document.getElementById('page2').style.display = 'none';
-  document.getElementById('page3').style.display = 'none';
-  document.getElementById('page4').style.display = 'none';
-  // show the one div that you sent to this function
-  document.getElementById(var1).style.display = 'block';
-}
+
