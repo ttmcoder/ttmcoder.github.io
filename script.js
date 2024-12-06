801dee8dd622367ba35f69a48788e00a3ac14e0f
ttmcoder <trumanjmeyer@gmail.com>
2024-12-06 15:00:25 -0500

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+function showit(var1) {
+  // Hide the one div that is shown
+  document.getElementById('page1').style.display = 'none';
+  document.getElementById('page2').style.display = 'none';
+  document.getElementById('page3').style.display = 'none';
+  document.getElementById('page4').style.display = 'none';
+  document.getElementById('page5').style.display = 'none';
+  document.getElementById('page6').style.display = 'none';
+  // show the one div that you sent to this function
+  document.getElementById(var1).style.display = 'inline';
+}
